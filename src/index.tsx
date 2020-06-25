ac680a5bf0de1ec33df6f262a3fd5a1437382c10
Johannes Klauss <klauss.johannes@gmail.com>
2020-06-25 18:03:23 +0200

    added standard libs

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createClient, Provider as UrqlProvider} from 'urql';
+
+const client = createClient({
+    url: 'https://graphql.fauna.com/graphql',
+    fetchOptions: () => ({
+        headers: {authorization: 'Bearer fnADo0-YxNACABRjBX8ER9FjEkUOzluRKoPYKhwh'},
+    }),
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <UrqlProvider value={client}>
+            <App/>
+        </UrqlProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
