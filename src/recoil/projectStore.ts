32056e7a39200cf080a335e6eb63b414063141f2
Johannes Klauss <klauss.johannes@gmail.com>
2020-07-14 21:25:18 +0200

    Added naive time signature.

--- src/recoil/projectStore.ts
+++ src/recoil/projectStore.ts
@@ -1,4 +1,5 @@
 import { atom } from 'recoil/dist';
+import { TimeSignature } from '../types/Music';
 
 const bpm = atom({
   key: 'project/bpm',
@@ -6,12 +7,24 @@ const bpm = atom({
 });
 
 // The project length in bars. At 120 bpm 240 bars equal 7:00 minutes.
+// TODO: THIS SHOULD BE AN ARRAY OF OBJECTS SO WE CAN CHANGE THE BPM DURING THE PROJECT.
 const length = atom({
   key: 'project/length',
   default: 240,
 });
 
+// The time signature of the project. "beat" is the upper nominal, "over" is the lower.
+// TODO: THIS SHOULD ALSO BE AN ARRAY SO WE CAN CHANGE THE TIME SIGNATURE DURING THE PROJECT.
+const timeSignature = atom<TimeSignature>({
+  key: 'project/timeSignature',
+  default: {
+    beat: 4,
+    over: 4,
+  },
+})
+
 export const projectStore = {
   bpm,
-  length
+  length,
+  timeSignature,
 };
---
+++ src/types/Music.ts
@@ -0,0 +1,4 @@
+export interface TimeSignature {
+  beat: number;
+  over: number;
+}
