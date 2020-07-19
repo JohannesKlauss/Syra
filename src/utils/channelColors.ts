2c07856196b1a1c4b8e4e36ffd9857398aaf6e16
Johannes Klauss <klauss.johannes@gmail.com>
2020-07-19 22:27:18 +0200

    Changed starting channel color to cyan

--- src/utils/channelColors.ts
+++ src/utils/channelColors.ts
@@ -15,7 +15,7 @@ import {
 } from '@material-ui/core/colors';
 
 export const channelColors = [
-  red[500], pink[500], purple[500], deepPurple[500], indigo[500], blue[500], cyan[500], teal[500], green[500], lime[500], yellow[500], amber[500], deepPurple[500],
-  red[900], pink[900], purple[900], deepPurple[900], indigo[900], blue[900], cyan[900], teal[900], green[900], lime[900], yellow[900], amber[900], deepPurple[900],
-  red[300], pink[300], purple[300], deepPurple[300], indigo[300], blue[300], cyan[300], teal[300], green[300], lime[300], yellow[300], amber[300], deepPurple[300],
+  cyan[500], red[500], pink[500], purple[500], deepPurple[500], indigo[500], blue[500], teal[500], green[500], lime[500], yellow[500], amber[500], deepPurple[500],
+  cyan[900], red[900], pink[900], purple[900], deepPurple[900], indigo[900], blue[900], teal[900], green[900], lime[900], yellow[900], amber[900], deepPurple[900],
+  cyan[300], red[300], pink[300], purple[300], deepPurple[300], indigo[300], blue[300], teal[300], green[300], lime[300], yellow[300], amber[300], deepPurple[300],
 ];
