ebc4de1e5c52198462414bdd218d2e783aa1adf7
Johannes Klauss <klauss.johannes@gmail.com>
2020-07-24 23:19:01 +0200

    Cleaned up plugin list architecture.

--- src/ui/molecules/Channels/HorizontalChannels/Strip/Input/InstrumentChannelInput.tsx
+++ src/ui/molecules/Channels/HorizontalChannels/Strip/Input/InstrumentChannelInput.tsx
@@ -1,20 +1,12 @@
 import React, { useContext } from 'react';
 import { ChannelContext } from '../../../../../../providers/ChannelContext';
-import { useRecoilState, useRecoilValue } from 'recoil/dist';
-import SoulInstrumentParameters from '../../../../SoulPlugin/SoulInstrumentParameters';
-import SoulPluginOverlay from '../../../../SoulPlugin/SoulPluginOverlay';
-import { soulPatchesStore } from '../../../../../../recoil/soulPatchesStore';
-import { channelStore } from '../../../../../../recoil/channelStore';
+import SoulPlugin from '../../../../SoulPlugin/SoulPlugin';
 
 function InstrumentChannelInput() {
   const channelId = useContext(ChannelContext);
-  const [activeInstrument, setActiveInstrument] = useRecoilState(channelStore.soulInstance(channelId));
-  const availableInstruments = useRecoilValue(soulPatchesStore.availableSoulInstruments);
 
   return (
-    <SoulPluginOverlay activePatch={activeInstrument} patchList={availableInstruments} setActivePatch={setActiveInstrument} isInstrument>
-      <SoulInstrumentParameters/>
-    </SoulPluginOverlay>
+    <SoulPlugin id={channelId} isInstrument={true}/>
   );
 }
 
--- src/ui/molecules/SoulPlugin/SoulPlugin.tsx
+++ src/ui/molecules/SoulPlugin/SoulPlugin.tsx
@@ -1,26 +1,65 @@
-import React, { useMemo } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import { useRecoilState, useRecoilValue } from 'recoil/dist';
 import ParameterList from './Parameters/ParameterList';
-import SoulPluginOverlay from './SoulPluginOverlay';
 import { channelStore } from '../../../recoil/channelStore';
 import { soulPatchesStore } from '../../../recoil/soulPatchesStore';
+import { Button, Container, MenuItem, Modal, Typography } from '@material-ui/core';
+import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
+import DropdownButton from '../../atoms/Buttons/DropdownButton';
+import { createSoulInstance } from '../../../soul/createSoulInstance';
 
 interface Props {
   id: string;
+  isInstrument?: boolean;
 }
 
-function SoulPlugin({id}: Props) {
-  const [activePlugin, setActivePlugin] = useRecoilState(channelStore.soulInstance(id));
-  const availablePlugins = useRecoilValue(soulPatchesStore.availableSoulPlugins);
+function SoulPlugin({id, isInstrument}: Props) {
+  const [activePlugin, setActivePlugin] = useRecoilState( channelStore.soulInstance(id));
+  const patchList = useRecoilValue(isInstrument ? soulPatchesStore.availableSoulInstruments : soulPatchesStore.availableSoulPlugins);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const modalContent = useMemo(() => (
-    activePlugin ? <ParameterList soulInstanceId={id} parameter={activePlugin.soulPatch.descriptor.parameters}/> : null
-  ), [id, activePlugin]);
+  const onChangePatch = useCallback(async (UID: string) => {
+    const patch = patchList.find(patch => patch.UID === UID);
+
+    if (patch) {
+      setActivePlugin(await createSoulInstance(patch, isInstrument));
+    }
+  }, [patchList, setActivePlugin, isInstrument]);
+
+  const menuItems = useMemo(() => patchList.map(patch => (
+    <MenuItem
+      key={patch.UID}
+      selected={activePlugin?.soulPatch.descriptor.description.UID === patch.UID}
+      onClick={async () => await onChangePatch(patch.UID)}
+    >
+      {patch.displayName}
+    </MenuItem>
+  )), [onChangePatch, activePlugin, patchList]);
+
+  const BypassButton = useMemo(() => (
+    <Button onClick={() => null}>
+      <PowerSettingsNewIcon/>
+    </Button>
+  ), []);
 
   return (
-    <SoulPluginOverlay activePatch={activePlugin} patchList={availablePlugins} setActivePatch={setActivePlugin}>
-      {modalContent}
-    </SoulPluginOverlay>
+    <>
+      {activePlugin && <Modal open={isModalOpen} onClose={() => setIsModalOpen(false)}>
+        <Container>
+          <ParameterList soulInstanceId={id} parameter={activePlugin.soulPatch.descriptor.parameters}/>
+        </Container>
+      </Modal>}
+      <DropdownButton
+        onClick={() => setIsModalOpen(activePlugin !== undefined)}
+        menuItems={menuItems}
+        fullWidth
+        small
+        menuPlacement={'right-start'}
+        prependingButton={BypassButton}
+      >
+        <Typography variant="overline" noWrap>{activePlugin?.soulPatch.descriptor.description.name}</Typography>
+      </DropdownButton>
+    </>
   );
 }
 
