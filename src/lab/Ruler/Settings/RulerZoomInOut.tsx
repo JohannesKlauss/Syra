22c910da50bf9e7561d9ca8f4ce16133ce2606a7
Johannes Klauss <klauss.johannes@gmail.com>
2020-07-28 11:53:28 +0200

    Added shortcuts for horizontal zoom.

--- src/lab/Ruler/Settings/RulerZoomInOut.tsx
+++ src/lab/Ruler/Settings/RulerZoomInOut.tsx
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Button, ButtonGroup } from '@material-ui/core';
 import ZoomInIcon from '@material-ui/icons/ZoomIn';
 import ZoomOutIcon from '@material-ui/icons/ZoomOut';
 import { useSetRecoilState } from 'recoil/dist';
 import { arrangeWindowStore } from '../../../recoil/arrangeWindowStore';
+import { useHotkeys } from 'react-hotkeys-hook';
 
 function RulerZoomInOut() {
   const setZoomLevel= useSetRecoilState(arrangeWindowStore.zoomLevel);
 
+  const zoomIn = useCallback(() => setZoomLevel(currVal => currVal + 1), [setZoomLevel]);
+  const zoomOut = useCallback(() => setZoomLevel(currVal => currVal - 1), [setZoomLevel]);
+
+  useHotkeys('command+right', e => {
+    e.preventDefault();
+    zoomIn();
+  });
+
+  useHotkeys('command+left', e => {
+    e.preventDefault();
+    zoomOut();
+  });
+
   return (
     <ButtonGroup variant={'text'} size={'small'}>
-      <Button size={'small'} onClick={() => setZoomLevel(currVal => currVal + 1)}>
+      <Button size={'small'} onClick={zoomIn}>
         <ZoomInIcon/>
       </Button>
-      <Button size="small" onClick={() => setZoomLevel(currVal => currVal - 1)}>
+      <Button size="small" onClick={zoomOut}>
         <ZoomOutIcon/>
       </Button>
     </ButtonGroup>
