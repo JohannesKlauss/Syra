6b95734f13757d22215b71c91b3e15e637c9994d
Johannes Klauss <klauss.johannes@gmail.com>
2020-08-04 16:38:08 +0200

    Log if audio context is running.

--- src/audioSetup.ts
+++ src/audioSetup.ts
@@ -5,6 +5,8 @@ export function audioSetup() {
     await Tone.start();
     await Tone.getContext().addAudioWorkletModule('worklets/SoulWasmAudioWorkletProcessor.js', 'soul-wasm-audio-worklet-processor');
 
+    console.log('AudioCtx is', Tone.getContext().state);
+
     document.body.removeEventListener('click', firstClick);
   }
 
