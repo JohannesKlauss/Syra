07cfe5bd7fe25b2657dca32b22a1586a9f842bef
Johannes Klauss <klauss.johannes@gmail.com>
2020-10-07 20:23:04 +0200

    Added a working GraphQL endpoint!

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,10 +1,44 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { TypeGraphQLModule } from "typegraphql-nestjs";
+import { PrismaClient } from "@prisma/client";
+import { ProjectRelationsResolver, UserRelationsResolver } from "../prisma/generated/type-graphql/resolvers/relations";
+import { Project, User } from "../prisma/generated/type-graphql/models";
+import { ProjectCrudResolver, UserCrudResolver } from "../prisma/generated/type-graphql/resolvers/crud";
+
+interface Context {
+  prisma: PrismaClient;
+}
+
+const prisma = new PrismaClient();
 
 @Module({
-  imports: [],
+  imports: [
+    TypeGraphQLModule.forRoot({
+      validate: false,
+      dateScalarMode: 'timestamp',
+      playground: {
+        endpoint: '/',
+      },
+      introspection: true,
+      path: '/',
+      emitSchemaFile: true,
+      context: (): Context => ({ prisma })
+    })
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    // Models
+    User,
+    Project,
+    // Relations
+    UserRelationsResolver,
+    ProjectRelationsResolver,
+    // Crud
+    UserCrudResolver,
+    ProjectCrudResolver,
+    // Custom
+  ]
 })
-export class AppModule {}
+export class AppModule {
+}
