dd4e19ae89e213d456f727e5d8a7e823f64b9815
Johannes Klauss <klauss.johannes@gmail.com>
2020-11-26 19:12:47 +0100

    Refactored web midi listener into a hook

--- services/syra-daw-fe/src/ui/screens/Editor.tsx
+++ services/syra-daw-fe/src/ui/screens/Editor.tsx
@@ -2,18 +2,19 @@ import React from 'react';
 import HorizontalChannelList from '../molecules/Channels/HorizontalChannels/HorizontalChannelList';
 import ArrangeWindow from '../molecules/ArrangeWindow/ArrangeWindow';
 import Piano from '../molecules/Piano/Piano';
-import WebMidiListener from '../molecules/Midi/WebMidiListener';
 import { editorStore } from '../../recoil/editorStore';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { useHotkeys } from 'react-hotkeys-hook';
 import Video from '../molecules/Video/Video';
 import { Box } from '@chakra-ui/react';
+import useWebMidiListener from "../../hooks/midi/useWebMidiListener";
 
 function Editor() {
   const showMixer = useRecoilValue(editorStore.showMixer);
   const showPianoRoll = useRecoilValue(editorStore.showPianoRoll);
   const [showVideo, setShowVideo] = useRecoilState(editorStore.showVideo);
 
+  useWebMidiListener();
   useHotkeys('v', () => setShowVideo(currVal => !currVal));
 
   return (
@@ -23,7 +24,6 @@ function Editor() {
           {showMixer && <HorizontalChannelList/>}
           {showPianoRoll && <Piano min={36} max={67}/>}
           {showVideo && <Video/>}
-          <WebMidiListener/>
         </Box>
       </>
   );
