63545896bd59f0de2adeeb109070ed8993cb616f
Johannes Klauss <klauss.johannes@gmail.com>
2020-12-08 18:04:37 +0100

    removed unneeded recoil callback

--- services/syra-daw-fe/src/hooks/fileImport/useImportMidiFile.ts
+++ services/syra-daw-fe/src/hooks/fileImport/useImportMidiFile.ts
@@ -2,15 +2,15 @@ import useCreateChannel from "../recoil/channel/useCreateChannel";
 import useCreateMidiRegion from "../recoil/region/useCreateMidiRegion";
 import { ChannelType } from "../../types/Channel";
 import { Midi } from "@tonejs/midi";
-import { useRecoilCallback } from "recoil";
 import useSetInstrument from "../recoil/channel/useSetInstrument";
+import { useCallback } from "react";
 
 export default function useImportMidiFile() {
   const createChannel = useCreateChannel();
   const createMidiRegion = useCreateMidiRegion();
   const setInstrument = useSetInstrument();
 
-  return useRecoilCallback(({set}) => async (file: File, importIndex: number, start: number = 0) => {
+  return useCallback(async (file: File, importIndex: number, start: number = 0) => {
     const midi = new Midi(await file.arrayBuffer());
 
     for (const track of midi.tracks) {
