76a7663b325deecce6d3eafa357732a4a6f40001
Johannes Klauss <klauss.johannes@gmail.com>
2021-01-14 14:52:21 +0100

    Added MIDI boundaries indicators

--- services/syra-daw-fe/src/ui/organisms/views/PianoRoll/MidiRegionIndicator.tsx
+++ services/syra-daw-fe/src/ui/organisms/views/PianoRoll/MidiRegionIndicator.tsx
@@ -1,12 +1,13 @@
-import { Box, Flex } from "@chakra-ui/react";
-import React, { useContext } from "react";
-import { regionStore } from "../../../../recoil/regionStore";
-import { useRecoilValue } from "recoil";
-import { RegionName, TopBar } from "../../../molecules/Region/AudioRegion/AudioRegion.styled";
-import { SiMidi } from 'react-icons/si';
+import {Box, Flex} from "@chakra-ui/react";
+import React, {useContext} from "react";
+import {regionStore} from "../../../../recoil/regionStore";
+import {useRecoilValue} from "recoil";
+import {RegionName, TopBar} from "../../../molecules/Region/AudioRegion/AudioRegion.styled";
+import {SiMidi} from 'react-icons/si';
 import useRegionColor from "../../../../hooks/ui/region/useRegionColor";
-import { RegionContext } from "../../../../providers/RegionContext";
+import {RegionContext} from "../../../../providers/RegionContext";
 import useMidiRegionWidth from "../../../../hooks/ui/region/useMidiRegionWidth";
+import useTicksToPixel from "../../../../hooks/tone/useTicksToPixel";
 
 interface Props {
 }
@@ -14,12 +15,37 @@ interface Props {
 const MidiRegionIndicator: React.FC<Props> = () => {
   const regionId = useContext(RegionContext);
   const name = useRecoilValue(regionStore.name(regionId));
+  const start = useRecoilValue(regionStore.start(regionId));
+  const ticksToPixel = useTicksToPixel();
   const color = useRegionColor(false);
   const regionWidth = useMidiRegionWidth();
 
   return (
-    <Box h={'20px'} w={`${regionWidth}px`} pos={'relative'}>
-      <TopBar color={color} rounded={4}>
+    <Box
+      h={'20px'}
+      w={`${regionWidth}px`}
+      pos={'absolute'}
+      left={`${ticksToPixel(start)}px`}
+      _before={{
+        backgroundColor: color,
+        content: '""',
+        display: 'inline-block',
+        height: '3000px',
+        top: '10px',
+        width: '1px',
+        position: 'absolute',
+      }}
+      _after={{
+        backgroundColor: color,
+        content: '""',
+        display: 'inline-block',
+        height: '3000px',
+        top: '10px',
+        width: '1px',
+        right: 0,
+        position: 'absolute',
+      }}>
+      <TopBar color={color} rounded={4} roundedBottom={0}>
         <Flex justify={'flex-start'} align={'center'} ml={2}>
           <SiMidi/>
           <RegionName color={color}>{name}</RegionName>
