8db760ef97e5b4e935dde97db3560b0908a3280b
Johannes Klauss <klauss.johannes@gmail.com>
2021-02-16 17:57:06 +0100

    Added ability to change tempo

--- services/syra-daw-fe/src/ui/molecules/Transport/Tempo.tsx
+++ services/syra-daw-fe/src/ui/molecules/Transport/Tempo.tsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import useTempoMapScheduler from '../../../hooks/tone/useTempoMapScheduler';
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Button } from '@chakra-ui/react';
+import { motion, PanInfo, useMotionValue } from "framer-motion";
+import { useSetRecoilState } from "recoil";
+import { projectStore } from "../../../recoil/projectStore";
 
 function Tempo() {
+  const setTempoMap = useSetRecoilState(projectStore.tempoMap);
   const currentTempo = useTempoMapScheduler();
+  const y = useMotionValue(0);
+  const lastOffset = useMotionValue(0);
+
+  const onYChanged = (e: MouseEvent, {offset}: PanInfo) => {
+    const roundedOffset = Math.round(-(offset.y / 5));
+
+    setTempoMap({0: Math.max(1, currentTempo + (roundedOffset - lastOffset.get()))});
+
+    lastOffset.set(roundedOffset);
+    y.set(0);
+  };
 
   return (
-    <Box>
-      <Text fontSize={'sm'}>{currentTempo}</Text>
-      <Text fontSize={'sm'}>BPM</Text>
+    <Box pos={'relative'}>
+      <motion.div drag={'y'} onDrag={onYChanged} onDragEnd={() => lastOffset.set(0)} dragMomentum={false} dragElastic={0} style={{y}}>
+        <Button variant={'ghost'} size={'sm'} cursor={'ns-resize'}>
+          {currentTempo} BPM
+        </Button>
+      </motion.div>
     </Box>
   );
 }
