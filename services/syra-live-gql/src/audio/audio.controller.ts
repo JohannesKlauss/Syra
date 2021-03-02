5efcf4c378008836c5edb2035a392772ad90e8ea
Johannes Klauss <klauss.johannes@gmail.com>
2021-03-02 10:55:55 +0100

    Remove job when completed

--- services/syra-live-gql/src/audio/audio.controller.ts
+++ services/syra-live-gql/src/audio/audio.controller.ts
@@ -1,20 +1,23 @@
-import { Controller, Param, Post, Query, Req, UseGuards } from "@nestjs/common";
-import { InjectQueue } from "@nestjs/bull";
-import { Queue } from "bull";
-import { CookieAuthGuard } from "../auth/cookie-auth.guard";
-import { Multipart } from "fastify-multipart";
-import { FilesService } from "../files/files.service";
-import { AudioTranscodeJob } from "../../types/AudioTranscodeJob";
+import { Controller, Param, Post, Query, Req, UseGuards } from '@nestjs/common';
+import { InjectQueue } from '@nestjs/bull';
+import { Queue } from 'bull';
+import { CookieAuthGuard } from '../auth/cookie-auth.guard';
+import { Multipart } from 'fastify-multipart';
+import { FilesService } from '../files/files.service';
+import { AudioTranscodeJob } from '../../types/AudioTranscodeJob';
 
 @Controller('audio')
 export class AudioController {
-  constructor(@InjectQueue('audio') private readonly audioQueue: Queue<AudioTranscodeJob>, private readonly filesService: FilesService) {}
+  constructor(
+    @InjectQueue('audio') private readonly audioQueue: Queue<AudioTranscodeJob>,
+    private readonly filesService: FilesService,
+  ) {}
 
   @Post('transcode/:projectId')
   @UseGuards(CookieAuthGuard)
   async upload(@Req() req, @Query() query, @Param('projectId') projectId) {
     const parts: Multipart[] = await req.parts();
-    let spacesObject: {id: string, location: string, name: string};
+    let spacesObject: { id: string; location: string; name: string };
 
     for await (const part of parts) {
       if (part.file) {
@@ -29,11 +32,17 @@ export class AudioController {
       }
     }
 
-    await this.audioQueue.add('transcode', {
-      spacesObject: spacesObject,
-      targetFormat: 'm4a',
-      projectId,
-    });
+    await this.audioQueue.add(
+      'transcode',
+      {
+        spacesObject: spacesObject,
+        targetFormat: 'm4a',
+        projectId,
+      },
+      {
+        removeOnComplete: true,
+      },
+    );
 
     return spacesObject;
   }
