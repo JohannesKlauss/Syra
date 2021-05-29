199700303823337fc4e0295f0c62a97de4553071
Johannes Klauss <klauss.johannes@gmail.com>
2021-05-29 17:23:33 +0200

    Added logs to audio controller

--- services/syra-live-gql/src/audio/audio.controller.ts
+++ services/syra-live-gql/src/audio/audio.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Param, Post, Query, Req, UseGuards } from "@nestjs/common";
+import { Controller, Logger, Param, Post, Query, Req, UseGuards } from "@nestjs/common";
 import { InjectQueue } from '@nestjs/bull';
 import { Queue } from 'bull';
 import { CookieAuthGuard } from '../auth/cookie-auth.guard';
@@ -9,6 +9,8 @@ import uniqid from 'uniqid';
 
 @Controller('audio')
 export class AudioController {
+  protected readonly logger = new Logger(AudioController.name);
+
   constructor(
     @InjectQueue('audio') private readonly audioQueue: Queue<AudioTranscodeJob>,
   ) {}
@@ -19,30 +21,50 @@ export class AudioController {
     const parts: Multipart[] = await req.parts();
     let jobId: string = uniqid();
 
+    this.logger.debug(`Set job id: ${jobId}`);
+
     for await (const part of parts) {
       if (part.file) {
         const tmpFileName = `audio-transcode-${jobId}`;
 
+        this.logger.debug(`tmpFileName: ${tmpFileName}`);
+
         if (!fs.existsSync(`${__dirname}/tmp`)) {
-          fs.mkdirSync(`${__dirname}/tmp`);
+          this.logger.debug(`tmp folder does not exist. Create...`);
+
+          try {
+            fs.mkdirSync(`${__dirname}/tmp`);
+          } catch (e) {
+            this.logger.error('Cannot create tmp directory.');
+            this.logger.error(e);
+          }
         }
 
-        part.file.pipe(fs.createWriteStream(`${__dirname}/tmp/${tmpFileName}`)).on('finish', () => {
-          this.audioQueue.add(
-            'transcode',
-            {
-              jobId,
-              originalMimeType: part.mimetype,
-              originalName: part.filename,
-              userId: req.user.id,
-              projectId,
-              tmpFileName,
-            },
-            {
-              removeOnComplete: true,
-            },
-          );
-        })
+        this.logger.debug(`Write file to tmp folder`);
+
+        part.file.pipe(fs.createWriteStream(`${__dirname}/tmp/${tmpFileName}`)).on('finish', async () => {
+          this.logger.debug(`Add to queue`);
+
+          try {
+            await this.audioQueue.add(
+              'transcode',
+              {
+                jobId,
+                originalMimeType: part.mimetype,
+                originalName: part.filename,
+                userId: req.user.id,
+                projectId,
+                tmpFileName,
+              },
+              {
+                removeOnComplete: true,
+              },
+            );
+          } catch (e) {
+            this.logger.error(`Cannot add job to queue`);
+            this.logger.error(e);
+          }
+        });
 
         break;
       }
